a3af939ea3a088a79430e4e3d1749eecba18ae51
Sudipto Das <eng.sudiptodas@gmail.com>
2026-05-08 11:04:04 +0600

    feat: add new image to  Lumina assets for  blog page

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import blogHero from '../assets/blogHero.jpg'
+import blogBody from '../assets/blogBody.jpg'
+import blogPost from '../assets/blogPost1.png'
+import blogPost2 from '../assets/blogPodt2.jpg'
+import blogPost3 from '../assets/blogPost3.jpg'
 
 const Blog = () => {
   return (
