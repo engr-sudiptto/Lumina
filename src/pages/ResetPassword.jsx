c3308d1dc1cac7181fe9ab8fc8b73771f9d5ee0c
Sudipto Das <eng.sudiptodas@gmail.com>
2026-05-10 15:11:44 +0600

    fix: resolve auto-focus issue in OTP on reset password page

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -9,21 +9,21 @@ const ResetPassword = () => {
   const [inputOtpFormVisible, setInputOtpFormVisible] = useState(true);
 
   const [verifiedUserInfo, setVerifiedUserInfo] = useState('');
-  const [newpassword, setNewPassword] = useState('')
+  const [newpassword, setNewPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   // ============= send otp button click function ===============
-  const sendOtpFormHandeler = e => {
+  const sendOtpFormHandler = e => {
     e.preventDefault();
     if (userInfo !== '') {
       setSendOtpFormVisible(false);
     }
   };
 
-  // =================== verify OTP form sunction =============
-  const handelChange = (e, index) => {
+  // =================== verify OTP form functionality =============
+  const handleChange = (e, index) => {
     const value = e.target.value;
     if (value === ' ' || isNaN(value)) return; // only number allow
 
@@ -38,17 +38,17 @@ const ResetPassword = () => {
     }
   };
 
-  // ------------ keyDown function ----------- 
-  const handelKeyDown = (e, index) => {
+  // ------------ keyDown function -----------
+  const handleKeyDown = (e, index) => {
     if (e.key === 'Backspace') {
       if (!inputOtp[index] && index > 0) {
         e.target.previousSibling.focus();
       }
     }
-  } 
+  };
 
-  // ------------- copy paste function ----------- 
-  const handlePaste = (e) => {
+  // ------------- copy paste function -----------
+  const handlePaste = e => {
     e.preventDefault();
     const pasteData = e.clipboardData.getData('text').slice(0, 6);
 
@@ -57,19 +57,22 @@ const ResetPassword = () => {
       const newOtp = pasteData.split('');
 
       // if number is less than 6, another box will be empty
-      const paddedOtp = [...newOtp, ...new Array(6 - newOtp.length).fill('')].slice(0, 6);
+      const paddedOtp = [
+        ...newOtp,
+        ...new Array(6 - newOtp.length).fill(''),
+      ].slice(0, 6);
 
       setInputOtp(paddedOtp);
 
       // move focus to the last box
       const lastBoxIndex = Math.min(newOtp.length, 5);
-      const inputs = e.target.parentElement.querySelectorAll('input')
-      inputs[lastBoxIndex].focus()
+      const inputs = e.target.parentElement.querySelectorAll('input');
+      inputs[lastBoxIndex].focus();
     }
   };
 
   // ------------ verify otp button click function -------------
-  const verifyOtpHandeler = e => {
+  const verifyOtpHandler = e => {
     e.preventDefault();
     if (inputOtp.join('').length === 6) {
       setInputOtpFormVisible(false);
@@ -78,18 +81,17 @@ const ResetPassword = () => {
     }
   };
 
-  // ================ reset password form handeler ================= 
-  const resetPasswordFormHandeler = (e) => {
-    e.preventDefault()
+  // ================ reset password form handeler =================
+  const resetPasswordFormHandler = e => {
+    e.preventDefault();
     if (verifiedUserInfo && newpassword && confirmPassword) {
       if (newpassword !== confirmPassword) {
         alert('Passwords do not match!'); // add backend functionality here
       } else {
-        navigate('/login')
+        navigate('/login');
       }
     }
-  }
-
+  };
 
   return (
     <div className="flex flex-col w-full h-screen items-center justify-center">
@@ -100,7 +102,7 @@ const ResetPassword = () => {
       {/* =========== send OTP form is here ===========  */}
       {sendOtpFormVisible && (
         <form
-          onSubmit={sendOtpFormHandeler}
+          onSubmit={sendOtpFormHandler}
           className=" w-[95%] max-w-170 p-4 shadow-2xl rounded-xl sm:px-15 sm:py-10"
         >
           <h1 className="text-3xl font-semibold text-center mt-3 text-gray-700">
@@ -153,11 +155,12 @@ const ResetPassword = () => {
           <div className="flex gap-3 mt-5 items-center justify-center">
             {inputOtp.map((data, index) => (
               <input
-                onChange={e => handelChange(e, index)}
-                onKeyDown={e => handelKeyDown(e, index)}
+                onChange={e => handleChange(e, index)}
+                onKeyDown={e => handleKeyDown(e, index)}
                 onPaste={handlePaste}
                 key={index}
                 value={data}
+                autoFocus={index === 0}
                 maxLength="1"
                 type="text"
                 className="w-10 h-10 sm:w-12 sm:h-12 xl:w-13 xl:h-13 border border-gray-300 rounded text-center content-center p-2 block font-bold text-2xl text-gray-700 outline-0 sm:mt-2 lg:mt-3 xl:mt-5 mt-1"
@@ -167,7 +170,7 @@ const ResetPassword = () => {
           </div>
 
           <button
-            onClick={verifyOtpHandeler}
+            onClick={verifyOtpHandler}
             className="block cursor-pointer w-full h-12 bg-blue-400/50 rounded-lg text-sm font-bold text-gray-700 hover:-translate-y-1 duration-300 hover:shadow-2xl mt-10 mb-5"
             type="submit"
           >
@@ -185,7 +188,7 @@ const ResetPassword = () => {
       {/* ================ password reset form =================  */}
       {!sendOtpFormVisible && !inputOtpFormVisible && (
         <form
-          onSubmit={resetPasswordFormHandeler}
+          onSubmit={resetPasswordFormHandler}
           className=" w-[95%] max-w-170 p-4 shadow-2xl rounded-xl sm:px-15 sm:py-10"
         >
           <h1 className="text-3xl font-semibold text-center mt-3 text-gray-700">
@@ -198,7 +201,7 @@ const ResetPassword = () => {
             Username or Email
           </label>
           <input
-            onChange={(e) => setVerifiedUserInfo(e.target.value)}
+            onChange={e => setVerifiedUserInfo(e.target.value)}
             className="block w-full h-12 mb-3 mt-2 border px-4 rounded-lg border-gray-300 outline-0 text-gray-600 tracking-wider text-sm sm:mb-5 sm:mt-3"
             type="text"
             placeholder="e.g.name@example.com"
@@ -208,7 +211,7 @@ const ResetPassword = () => {
             New Password
           </label>
           <input
-            onChange={(e) => setNewPassword(e.target.value)}
+            onChange={e => setNewPassword(e.target.value)}
             className="block w-full h-12 mb-3 mt-2 border px-4 rounded-lg border-gray-300 outline-0 text-gray-600 tracking-wider text-sm sm:mb-4 sm:mt-3"
             type="password"
             placeholder="Password"
@@ -218,9 +221,9 @@ const ResetPassword = () => {
             Confirm Password
           </label>
           <input
-            onChange={(e) => setConfirmPassword(e.target.value)}
+            onChange={e => setConfirmPassword(e.target.value)}
             className="block w-full h-12 mb-3 mt-2 border px-4 rounded-lg border-gray-300 outline-0 text-gray-600 tracking-wider text-sm sm:mb-10 sm:mt-3"
-            type='password'
+            type="password"
             placeholder="Confirm Password"
             required
           />
