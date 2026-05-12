4ddfc769cecd20e5c7e8fc219d6f72249a15a970
Sudipto Das <eng.sudiptodas@gmail.com>
2026-05-12 13:28:48 +0600

    feat: implement Navbar for the authenticated users

--- src/afterLoginPages/Profile.jsx
+++ src/afterLoginPages/Profile.jsx
@@ -1,59 +1,65 @@
 import React from 'react';
 import profileImage from '../assets/userTeam3.jpg';
 import profileBackgroundImage from '../assets/blogPost1.png';
+import AfterLoginNav from '../components/AfterLoginNav';
 
 const Profile = () => {
   return (
-    <div className="w-full max-w-300 m-auto">
-      <div className="w-full h-60 lg:h-70">
-        <img
-          className="w-full h-full object-cover"
-          src={profileBackgroundImage}
-        />
-      </div>
-
-      <div className="lg:flex lg:gap-10 lg:pl-5 xl:p-8">
-        {/* profile info section */}
-        <div className="p-5 lg:basis-1/3">
-          <div className="w-30 h-30 lg:w-35 lg:h-35 xl:w-40 xl:h-40 -mt-21 lg:-mt-23 xl:-mt-35 relative">
-            <img
-              className="w-full h-full object-cover rounded-full"
-              src={profileImage}
-            />
-            <span className="block w-4 h-4 xl:w-5 xl:h-5 rounded-full bg-green-500 absolute bottom-2 right-3 lg:bottom-3 lg:right-4"></span>
-          </div>
-          <h3 className="text-2xl font-semibold pl-2 mt-2 lg:text-3xl">
-            User Name
-          </h3>
-          <p className="text-xs text-gray-500 pl-2 lg:text-sm">@username_01</p>
-          <p className="text-sm  pl-2  mt-3 text-gray-500 font-semibold lg:text-base">
-            title: Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          </p>
-          {/* ------- following and followers --------  */}
-          <div className='flex w-[80%] lg:w-[70%] xl:w-[65%] m-auto lg:m-0 justify-between p-5 lg:p-0 lg:mt-4 lg:px-3'>
-            <div className='flex flex-col justify-center items-center'>
-              <h3 className='font-bold text-sm text-gray-500'>Followers</h3>
-              <p className='text-gray-400 font-semibold'>256</p>
-            </div>
-            <span className='block w-0.5 h-15 bg-gray-300'></span>
-            <div className='flex flex-col justify-center items-center'>
-              <h3 className='font-bold text-sm text-gray-500'>Following</h3>
-              <p className='text-gray-400 font-semibold'>18</p>
-            </div>
-          </div>
+    <div>
+      <AfterLoginNav/>
+      <div className="w-full max-w-300 m-auto pt-23 sm:pt-28 ">
+        <div className="w-full h-45 sm:h-60 lg:h-70">
+          <img
+            className="w-full h-full object-cover"
+            src={profileBackgroundImage}
+          />
         </div>
 
-        {/* ========== profile about section ========= */}
-        <div className="p-5 lg:basis-2/3 lg:mt-3">
-          <h3 className="text-lg pl-2 font-semibold text-gray-600 lg:text-2xl">
-            About
-          </h3>
-          <p className="text-sm pl-2 text-gray-400 lg:text-base">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa
-            perferendis beatae esse ipsum dolorem! Recusandae laborum optio
-            numquam quibusdam rerum explicabo eius nulla, minima magni molestias
-            error ut itaque veritatis.
-          </p>
+        <div className="lg:flex lg:gap-10 lg:pl-5 xl:p-8">
+          {/* profile info section */}
+          <div className="p-5 lg:basis-1/3">
+            <div className="w-30 h-30 lg:w-35 lg:h-35 xl:w-40 xl:h-40 -mt-21 lg:-mt-23 xl:-mt-35 relative">
+              <img
+                className="w-full h-full object-cover rounded-full"
+                src={profileImage}
+              />
+              <span className="block w-4 h-4 xl:w-5 xl:h-5 rounded-full bg-green-500 absolute bottom-2 right-3 lg:bottom-3 lg:right-4"></span>
+            </div>
+            <h3 className="text-2xl font-semibold pl-2 mt-2 lg:text-3xl">
+              User Name
+            </h3>
+            <p className="text-xs text-gray-500 pl-2 lg:text-sm">
+              @username_01
+            </p>
+            <p className="text-sm  pl-2  mt-3 text-gray-500 font-semibold lg:text-base">
+              title: Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+            {/* ------- following and followers --------  */}
+            <div className="flex w-[80%] lg:w-[70%] xl:w-[65%] m-auto lg:m-0 justify-between p-5 lg:p-0 lg:mt-4 lg:px-3">
+              <div className="flex flex-col justify-center items-center">
+                <h3 className="font-bold text-sm text-gray-500">Followers</h3>
+                <p className="text-gray-400 font-semibold">256</p>
+              </div>
+              <span className="block w-0.5 h-15 bg-gray-300"></span>
+              <div className="flex flex-col justify-center items-center">
+                <h3 className="font-bold text-sm text-gray-500">Following</h3>
+                <p className="text-gray-400 font-semibold">18</p>
+              </div>
+            </div>
+          </div>
+
+          {/* ========== profile about section ========= */}
+          <div className="p-5 lg:basis-2/3 lg:mt-3">
+            <h3 className="text-lg pl-2 font-semibold text-gray-600 lg:text-2xl">
+              About
+            </h3>
+            <p className="text-sm pl-2 text-gray-400 lg:text-base">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa
+              perferendis beatae esse ipsum dolorem! Recusandae laborum optio
+              numquam quibusdam rerum explicabo eius nulla, minima magni
+              molestias error ut itaque veritatis.
+            </p>
+          </div>
         </div>
       </div>
     </div>
---
+++ src/components/AfterLoginNav.jsx
@@ -0,0 +1,155 @@
+import React from 'react'
+import luminaLogo from '../assets/lumina.png'
+import { Link } from 'react-router-dom'
+
+const AfterLoginNav = () => {
+  return (
+    <div className="block bg-white w-full max-w-300 fixed top-0 left-1/2 -translate-x-1/2">
+      <div className="flex justify-between px-3 pt-2 h-12 items-center sm:px-5">
+        <Link>
+          <img className="w-30" src={luminaLogo} alt="Lumina" />
+        </Link>
+        <nav>
+          <ul className="flex gap-4">
+            <Link>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-7 text-gray-500"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+                />
+              </svg>
+            </Link>
+            <Link>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-7 text-gray-500"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 0 1-.825-.242m9.345-8.334a2.126 2.126 0 0 0-.476-.095 48.64 48.64 0 0 0-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0 0 11.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155"
+                />
+              </svg>
+            </Link>
+          </ul>
+        </nav>
+      </div>
+
+      <div className="flex w-full justify-between px-3 h-10 sm:h-13 items-center py-6 sm:px-10 sm:py-8 lg:w-[80%] xl:w-[70%] lg:m-auto">
+        <Link>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="size-7 text-gray-500 sm:size-8"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+            />
+          </svg>
+        </Link>
+
+        <Link>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="size-7 text-gray-500 sm:size-8"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z"
+            />
+          </svg>
+        </Link>
+
+        <Link>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="size-7 text-gray-500 sm:size-8"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"
+            />
+          </svg>
+        </Link>
+
+        <Link>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="size-7 text-[#22709370] sm:size-8"
+          >
+            <path
+              fillRule="evenodd"
+              d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </Link>
+
+        <Link>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="size-7 text-gray-500 sm:size-8"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0M3.124 7.5A8.969 8.969 0 0 1 5.292 3m13.416 0a8.969 8.969 0 0 1 2.168 4.5"
+            />
+          </svg>
+        </Link>
+
+        <Link>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="size-7 text-gray-500 sm:size-8"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+            />
+          </svg>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default AfterLoginNav
